1a5440ea62f691faeacc3efdf45cb0421a479783
Zeenat100 <siddzee100@gmail.com>
2020-10-18 08:12:41 -0700

    Employee part time daily wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,13 +3,16 @@ public class EmpWage
 	public static void main (String args[])
 	{
 	System.out.println("Welcome to employee wage computation");
-	int IS_FULL_TIME = 1;
+	int IS_PART_TIME = 1;
+	int IS_FULL_TIME = 2;
 	int emp_rate_per_hr=20;
 	int empHrs=0;
 	int empWage=0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
 	if (empCheck == IS_FULL_TIME)
 		empHrs=8;
+	else if (empCheck == IS_PART_TIME)
+		empHrs=4;
 	empWage = empHrs * emp_rate_per_hr;
 	System.out.println("Employee daily Wage: " +empWage);
 	}
