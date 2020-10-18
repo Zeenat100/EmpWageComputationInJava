b6eeeb1354f13552e1f6abbd62c8ffc5cdc00c57
Zeenat100 <siddzee100@gmail.com>
2020-10-18 08:06:19 -0700

    Employee daily wage

--- EmpWage.java
+++ EmpWage.java
@@ -4,10 +4,13 @@ public class EmpWage
 	{
 	System.out.println("Welcome to employee wage computation");
 	int IS_FULL_TIME = 1;
+	int emp_rate_per_hr=20;
+	int empHrs=0;
+	int empWage=0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
 	if (empCheck == IS_FULL_TIME)
-		System.out.println("Employee is present");
-	else
-		System.out.println("Employee is absent");
+		empHrs=8;
+	empWage = empHrs * emp_rate_per_hr;
+	System.out.println("Employee daily Wage: " +empWage);
 	}
 }
