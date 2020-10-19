8b75a16c70d3ed24e92fcfcf99fa8943366c7341
Zeenat100 <siddzee100@gmail.com>
2020-10-19 04:24:31 -0700

    Employee Wage upto max hours or max days

--- EmpWage.java
+++ EmpWage.java
@@ -4,11 +4,15 @@ public class EmpWage
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 	public static void main (String args[])
 	{
 		System.out.println("Welcome to employee wage computation");
-		int empHrs=0, empWage=0, totalEmpWage =0;
-		for (int day=0 ; day < NUM_OF_WORKING_DAYS ; day++){
+		int totalEmpHrs=0, totalEmpWage =0, totalWorkingDays=0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+		{
+			int empHrs=0;
+			totalWorkingDays++;
         		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch ((int)empCheck){
 				case IS_FULL_TIME:
@@ -20,7 +24,8 @@ public class EmpWage
 				default:
 					empHrs=0;
 			}
-		empWage = empHrs * EMP_RATE_PER_HR;
+		totalEmpHrs += empHrs;
+		int empWage = empHrs * EMP_RATE_PER_HR;
 		totalEmpWage += empWage;
 		System.out.println("Employee daily Wage: " + empWage);
 		}
